30886da01dac72622e112dcaa2589bbc8c4d7d9a
IGGAMEMAKER <23i03g@mail.ru>
2016-12-12 10:12:45 +0300

    made contacts page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+function render(page) {
+  return function(req, res) {
+    res.render(page);
+  }
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+
+router.get('/contacts', render('pages/contacts'));
+// router.get('/contacts', render('contacts'));
 
 module.exports = router;
